da1660db0e8183166bba0f22f63de4ae4c11346c
Stacyomwando <annaalice82@gmail.com>
2018-10-22 19:50:38 +0300

    Updated files

--- controllers/bk.php
+++ controllers/bk.php
@@ -1,29 +1,32 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
 class bk extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('booking');
-		$this->load->model('book');
+public function __construct(){
+
+        parent::__construct();
+  			$this->load->helper('url');
+  	 		$this->load->model('book_model');
+  	 		$this->load->library('session');
+
+        
+
+}
+
+	public function index(){
+		$this->load->view('dashboard.php');
+
 	}
+
+	public function booking_view()
+	{
+		$id=$_GET["id"];
+		$this->load->view('booking.php');
+		
+	}
+	
 	public function book(){
+		
 		$data = array(
 			'firstname'=>$this->input->post('firstname'),
 			'idno'=>$this->input->post('idno'),
@@ -31,6 +34,17 @@ class bk extends CI_Controller {
 			'address'=>$this->input->post('address'),
 			'date'=>$this->input->post('date'),
 		);
-		$result = $this->book->book($data);
+		
+	// print_r($data);
+	$this->book_model->book($data);
+	$this->load->view('dashboard.php');
+
 	}
+
+	public function user_logout(){
+
+  $this->session->sess_destroy();
+  redirect('user/login_view', 'refresh');
 }
+}
+?>
